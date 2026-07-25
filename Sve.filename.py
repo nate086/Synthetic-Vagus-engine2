8bb1b3879f020e1b7d32e1ae6ce50c2d8f851b33
nate086 <highn8086@gmail.com>
2026-07-25 04:15:19 -0700

    Add initial implementation of Synthetic Vagus Engine

---
+++ Sve.filename.py
@@ -0,0 +1,18 @@
+"""
+Synthetic Vagus Engine (SVE)
+Biomimetic Activation Steering for AI Safety
+"""
+
+from sve.autonomic import AutonomicStateMachine, AutonomicState, AutonomicStatus
+from sve.steering import SyntheticVagusHook
+from sve.monitor import NeuroceptionMonitor
+from sve.engine import SyntheticVagusEngine
+
+__all__ = [
+    "AutonomicStateMachine",
+    "AutonomicState",
+    "AutonomicStatus",
+    "SyntheticVagusHook",
+    "NeuroceptionMonitor",
+    "SyntheticVagusEngine",
+]
