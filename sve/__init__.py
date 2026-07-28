f9df0c508048d9d9b075cad0d78936a6ceb17fc3
nate086 <highn8086@gmail.com>
2026-07-27 18:21:41 -0700

    Export public API from sve package

--- sve/__init__.py
+++ sve/__init__.py
@@ -1,6 +1,15 @@
-"""
-Synthetic Vegas Engine 2
-Biomimetic activation-steering framework for LLM safety.
-"""
+from .autonomic import AutonomicEngine, AutonomicStateMachine, AutonomicStatus
+from .steering import SyntheticVagusHook
+from .monitor import NeuroceptionMonitor
+from .engine import SyntheticVagusEngine
+
+__all__ = [
+    "AutonomicEngine",
+    "AutonomicStateMachine",
+    "AutonomicStatus",
+    "SyntheticVagusHook",
+    "NeuroceptionMonitor",
+    "SyntheticVagusEngine",
+]
 
 __version__ = "0.1.0"
