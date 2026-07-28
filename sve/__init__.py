63e6b0bcadd48e1a54ae095c9bbb3fcf9b3ea569
nate086 <highn8086@gmail.com>
2026-07-27 22:02:33 -0700

    Refactor imports and update __all__ in __init__.py

--- sve/__init__.py
+++ sve/__init__.py
@@ -1,15 +1,6 @@
-from .autonomic import AutonomicEngine, AutonomicStateMachine, AutonomicStatus
-from .steering import SyntheticVagusHook
-from .monitor import NeuroceptionMonitor
-from .engine import SyntheticVagusEngine
+"""Synthetic Vagus Engine (sve) package."""
 
-__all__ = [
-    "AutonomicEngine",
-    "AutonomicStateMachine",
-    "AutonomicStatus",
-    "SyntheticVagusHook",
-    "NeuroceptionMonitor",
-    "SyntheticVagusEngine",
-]
+from sve.autonomic import AutonomicEngine
+from sve.signals import SignalGenerator
 
-__version__ = "0.1.0"
+__all__ = ["AutonomicEngine", "SignalGenerator"]
